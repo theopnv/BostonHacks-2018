7e8ef4b4c3bf62384c63a6f66c7bca97d329ef0c
debray_v <victor.debray@epitech.eu>
2018-11-11 00:28:26 -0500

    statics

---
+++ constants/Spotify_keys.js
@@ -0,0 +1,5 @@
+export default {
+  client_id: '92ac409ca3e745a8a33b3b574453ebfa', // Your client id
+  client_secret: '98f1c6671c1048fa831c8965bfa72888', // Your secret
+  redirect_uri: 'https://example.com' // Your redirect uri
+};
